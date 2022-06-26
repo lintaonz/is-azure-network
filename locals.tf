05980e78e9782f44cc6ad69f44a7b875227c1255
Tao Lin <316456@twgroup.co.nz>
2022-06-27 11:45:35 +1200

    feat: PPD-10958 locals support DR env

--- locals.tf
+++ locals.tf
@@ -24,19 +24,19 @@ locals {
       route = [
         {
           name           = "blackhole_fw_mgmt"
-          address_prefix = "10.18.2.0/24"
+          address_prefix = var.environment == "prod" ? "10.18.2.0/24" : "10.19.2.0/24"
           next_hop_type  = "None"
         },
         {
           name           = "blackhole_fw_public"
-          address_prefix = "10.18.3.0/24"
+          address_prefix = var.environment == "prod" ? "10.18.3.0/24" : "10.19.3.0/24"
           next_hop_type  = "None"
         },
         {
           name                   = "default_2_LB_obewfw"
           address_prefix         = "0.0.0.0/0"
           next_hop_type          = "VirtualAppliance"
-          next_hop_in_ip_address = "10.18.4.21"
+          next_hop_in_ip_address = var.environment == "prod" ? "10.18.4.21" : "10.19.4.21"
         }
       ],
       tags = {
@@ -46,7 +46,7 @@ locals {
   }
   route_tables                      = var.provision_default_route_table ? merge(local.default_route_table, var.route_tables) : var.route_tables
   route_tables_ignore_route_changes = { for k, v in local.route_tables : k => v if lookup(v, "ignore_route_changes", false) }
-  route_tables_normal               = { for k, v in local.route_tables : k => v if ! lookup(v, "ignore_route_changes", false) }
+  route_tables_normal               = { for k, v in local.route_tables : k => v if !lookup(v, "ignore_route_changes", false) }
 
   vnet_rg_name = coalesce(var.vnet_rg_name, "${var.environment}-vnet-rg")
 
